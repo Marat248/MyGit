1c32e52200b0b801c4c0dbb8961a18f6cbab1cd2
Timur Sayapov <timur@improvation.us>
2022-10-12 11:49:20 -0400

    Lesson "Function"

--- Project_1/about.php
+++ Project_1/about.php
@@ -1,11 +1,8 @@
 <?php
-    $title = "Main page";
+    $title = "About us";
     require "blocks/header.php";
 ?>
-<h1>Main page</h1>
+<h1>About us</h1>
 <?php
     require "blocks/footer.php";
 ?>
-
- 
-
