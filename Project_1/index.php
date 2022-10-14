b470f90b8a799e271762cdc280dbbdd0479713f0
Timur Sayapov <timur@improvation.us>
2022-10-14 16:39:13 -0400

    Lesson "Function"

--- Project_1/index.php
+++ Project_1/index.php
@@ -4,40 +4,16 @@
 ?>
 <h1>Main page</h1>
 <?php
-    $lis = [5, 7, 3, 6, 7, 8];
-    unset($lis[1]);
-    $lis = array_values($lis);
-    rsort($lis);
 
-    $arr = array_slice($lis, 2, 2);
-    print_r($arr);
-    echo '<br>';
+    $message = "message";
+    $to = "admin@proger.com";
+    $from = "example@proger.com";
+    $subject = "message subject";
 
-    $arr_1 = [5, 7];
-    $arr_2 = [6, 8, 9];
-    $arr_3 = array_merge($arr_1, $arr_2);
-    print_r($arr_3);
-    echo '<br>';
-//    if (in_array(30, $lis) == "")
-//        echo 'not founde ';
-//    else
-//        echo 'found ';
-//    shuffle($lis);
-    print_r($lis);
-    echo '<br>';
+    $subject = "=?utf-8?B?".base64_encode($subject)."?=";
+    $headers = "From: $from\r\nReply-to: $from\r\nContent-type:text/plain; charset=utf-8\r\n";
 
-    $x = "10";
-    echo gettype($x).'<br>';
-    echo is_numeric($x).'<br>';
-    echo is_integer($x).'<br>';
-
-    $str = "Example";
-    echo strpos($str, 'a').'<br>';
-
-    $words = "john, bob ,alex";
-    $arr_words = explode(",", $words);
-    print_r($arr_words).'<br>';
-    echo '<br>'.implode(" | ", $arr_words);
+    mail($to, $subject, $message, $headers);
 
     require_once "blocks/footer.php";
 ?>
