dd4a6d080fcda0250e0133d71da5306c5e6fe616
Timur Sayapov <timur@improvation.us>
2022-10-17 16:48:14 -0400

    Lesson "Function"

--- Project_1/blocks/footer.php
+++ Project_1/blocks/footer.php
@@ -1,3 +1,4 @@
-<footer>All rights of protection &copy; 2050</footer>
+<footer class="mt-5">All rights of protection &copy; 2050</footer>
+</div>
 </body>
 </html>
--- Project_1/index.php
+++ Project_1/index.php
@@ -1,20 +1,9 @@
 <?php
     $title = "Main page";
-    require "blocks/header.php";
+    require_once "blocks/header.php";
 ?>
 <h1>Main page</h1>
 <?php
-
-    $message = "message";
-    $to = "admin@proger.com";
-    $from = "example@proger.com";
-    $subject = "message subject";
-
-    $subject = "=?utf-8?B?".base64_encode($subject)."?=";
-    $headers = "From: $from\r\nReply-to: $from\r\nContent-type:text/plain; charset=utf-8\r\n";
-
-    mail($to, $subject, $message, $headers);
-
     require_once "blocks/footer.php";
 ?>
 
